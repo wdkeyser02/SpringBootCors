c5e071366923652dd629a53b5b12fee7cff043ee
willydekeyser <ma074172@skynet.be>
2023-04-15 12:53:36 +0200

    Setup Spring Boot Cors

---
+++ SpringBootCors/src/main/java/willydekeyser/SpringBootCorsApplication.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCorsApplication.class, args);
+	}
+
+}
